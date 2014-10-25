f48068f5b6dd846e1167779d63d9518aee6eecb8
bhargavn <nilagiribhargav@gmail.com>
2014-10-25 21:59:41 +0530

    test 1

---
+++ mail/contact_me.php
@@ -0,0 +1,44 @@
+<?php
+$servername = "localhost";
+$username = "bhargav";
+$password = "12345";
+$dbname = "bhargav";
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['phone']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+//$sql = "INSERT INTO contact_details (name,phone,email,message)
+//VALUES ("$name","$phone","$email_address","$message")";
+$sql = "INSERT INTO contact_details (name,phone,email,message) values('$name','$phone','$email_address','$message')";
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+mysqli_close($conn);
+
+// Create the email and send the message
+$to = 'nilagiribhargav@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+return true;			
+?>
